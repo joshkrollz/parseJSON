1f002b1f417ff9823a00e6997f03d3dbcb21e418
joshkrollz <joshua.karolus@cuw.edu>
2015-11-09 16:43:43 -0600

    first commit

---
+++ Exit.java
@@ -0,0 +1,27 @@
+
+public class Exit 
+{
+	public class Exit 
+	{
+		private String name;
+		private Room destination;
+		
+		public Exit(String name, Room destination)
+		{
+			this.name = name;
+			this.destination = destination;
+		}
+
+		public String getName() 
+		{
+			return name;
+		}
+
+		public Room getDestination() 
+		{
+			return destination;
+		}
+		
+		
+	}
+}
